2530eb821bf29ec70f212da6a9dd381f975731a6
mmonahan <dr.mirkops@gmail.com>
2019-11-14 21:02:06 +0300

    check creat_file. Start fill new_file

--- validation.c
+++ validation.c
@@ -6,7 +6,7 @@
 /*   By: mmonahan <mmonahan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 11:37:56 by mmonahan          #+#    #+#             */
-/*   Updated: 2019/11/12 20:16:57 by mmonahan         ###   ########.fr       */
+/*   Updated: 2019/11/14 20:55:05 by mmonahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 
 static char	*creat_name_file(const char *old_name)
 {
-	int 	length;
+	int		length;
 	char	*new_name;
 
 	length = ft_strlen(old_name);
-	new_name = ft_memalloc(length + 2);
+	new_name = ft_memalloc(length + 3);
 	ft_strcpy(new_name, old_name);
 	new_name[length - 1] = 'c';
 	new_name[length] = 'o';
@@ -30,32 +30,68 @@ static char	*creat_name_file(const char *old_name)
 	return (new_name);
 }
 
+static int	creat_fill_file(const char *namefile, const char *file)
+{
+	char *new_name;
+	int fd;
+
+	new_name = creat_name_file(namefile);
+	fd = creat(new_name, 755);
+	free(new_name);
+	if (fd < 1)
+		return (ERR_NO_CREAT_FILE);
+	// записываем команды в 16-и ричной системе счисления в файл
+	// *** супер функция записи строки в этот файл
+	// fill_new_file(fd, file);
+	if (file)
+	{
+		;
+	}
+	int i = 0;
+	int len = 0;
+	while (file[i])
+	{
+		while (file[len] != '\n')
+			len++;
+		write(fd, &file, len);
+		i++;
+		len = 0;
+		// БРЕД! Переписать нахрен!)
+	}
+	close(fd);
+	return (ERR_NORM);
+}
+
 /*
 **	Возвращает 0, если имя файла нормальное, в противном случае 1
 */
-int		check_name_file(const char *name_file)
+
+static int	check_name_file(const char *name_file)
 {
-	int 	length;
+	int	length;
 
 	length = ft_strlen(name_file);
 	if (name_file[length - 1] != 's')
 		return (1);
 	if (name_file[length - 2] != '.')
 		return (1);
-	if (!ft_isdigit(name_file[length - 3]) && ft_isalpha(name_file[length]))
+	if (!ft_isdigit(name_file[length - 3]) &&
+		!ft_isalpha(name_file[length - 3]))
 		return (1);
 	return (0);
 }
 
-int validation(char *namefile)
+/*
+**	Валидация файла
+*/
+
+int	validation(char *namefile)
 {
 	int		fd_open;
-	int		fd_creat;
-	char	*name;
 	char 	*string;
 	char 	*file;
 
-	string = ft_memalloc(1);
+	string = NULL;
 	file = ft_memalloc(1);
 	if (check_name_file(namefile))
 		return (ERR_BAD_NAME_FILE);
@@ -65,26 +101,16 @@ int validation(char *namefile)
 	// сохраненяем файл в массив построчно и проверяем его
 	while (get_next_line(fd_open, &string))
 	{
-		// check_string(true)
+		// check_string(string);
 		file = ft_str_rejoin(file, string);
 		ft_strdel(&string);
 		file = ft_str_rejoin(file, "\n");
 	}
 	ft_strdel(&string);
-
-	printf("%s\n", file);
 	close(fd_open);
-/*
-**	это надо в другую функцию
-**	т.е. создание и  заполнение файла только после проюождения валидации
-*/
-	name = creat_name_file(namefile);
-	fd_creat = creat(name, 755);
-	free(name);
-	if (fd_creat < 1)
-		return (ERR_NO_CREAT_FILE);
-	// записываем команды в 16-и ричной системе счисления в файл
 
-	close(fd_creat);
+	//создание и заполнение файла только после проюождения валидации
+	//можно вообще в отдельный файл жахнуть
+	creat_fill_file(namefile, file);
 	return (ERR_NORM);
 }
