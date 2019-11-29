43f82c4e085ac42f7d4b6ae180654c32e5b26a1a
mmonahan <dr.mirkops@gmail.com>
2019-11-29 21:03:42 +0300

    start get_instruction 5

--- asm.h
+++ asm.h
@@ -6,7 +6,7 @@
 /*   By: mmonahan <mmonahan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 20:40:49 by mmonahan          #+#    #+#             */
-/*   Updated: 2019/11/27 20:36:24 by mmonahan         ###   ########.fr       */
+/*   Updated: 2019/11/29 20:38:33 by mmonahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,27 @@ typedef struct	s_file
 	int 		fd_open;
 	int 		fd_creat;
 }				t_file;
+
+static t_operation	op_tab[17] =
+{
+		{LIVE, 1, {T_DIR}, 1, 10, 0, 0},
+		{LD, 2, {T_DIR | T_IND, T_REG}, 2, 5, 1, 0},
+		{ST, 2, {T_REG, T_IND | T_REG}, 3, 5, 1, 0},
+		{ADD, 3, {T_REG, T_REG, T_REG}, 4, 10, 1, 0},
+		{SUB, 3, {T_REG, T_REG, T_REG}, 5, 10, 1, 0},
+		{AND, 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG}, 6, 6, 1, 0},
+		{OR, 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 7, 6, 1, 0},
+		{XOR, 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 8, 6, 1, 0},
+		{ZJMP, 1, {T_DIR}, 9, 20, 0, 1},
+		{LDI, 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25, 1, 1},
+		{STI, 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25, 1, 1},
+		{FORK, 1, {T_DIR}, 12, 800, 0, 1},
+		{LLD, 2, {T_DIR | T_IND, T_REG}, 13, 10, 1, 0},
+		{LLDI, 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 50, 1, 1},
+		{LFORK, 1, {T_DIR}, 15, 1000, 0, 1},
+		{FFF, 1, {T_REG}, 16, 2, 1, 0},
+		{0, 0, {0}, 0, 0, 0, 0}
+};
 
 void			put_exception(int error);
 char			*get_instruction(t_header *header, const char *string);
--- get_instruction.c
+++ get_instruction.c
@@ -6,7 +6,7 @@
 /*   By: mmonahan <mmonahan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 20:17:13 by mmonahan          #+#    #+#             */
-/*   Updated: 2019/11/28 21:01:53 by mmonahan         ###   ########.fr       */
+/*   Updated: 2019/11/29 21:02:03 by mmonahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,25 @@ int	check_name(t_header *header, const char *string, const char *instr)
 		i++;
 	while (*instr)
 		if (string[i++] != *instr++)
-			return (ERR_BEAD_HEADER + 1);
+			return (ERR_BEAD_HEADER + 10);
 	if (ft_isspace(string[i]))
 		while (ft_isspace(string[i]))
 			i++;
 	if (string[i++] == '"')
 		while (string[i] != '"')
+		{
 			header->prog_name[k++] = string[i++];
+			if (k == PROG_NAME_LENGTH)
+				return (ERR_BEAD_HEADER + 20);
+			if (string[i] == '\0')
+				return (ERR_BEAD_HEADER + 30);
+		}
 	else
-		return (ERR_BEAD_HEADER + 2); // продолжить от сюда!!!! 2019.11.28
+		return (ERR_BEAD_HEADER + 40);
+//	while (ft_isspace(string[i]))
+//		i++;
+	if (string[i + 1] != '\0')
+		return (ERR_BEAD_HEADER + 50);
 	return (ERR_NORM);
 }
 
@@ -41,27 +51,31 @@ int	check_name(t_header *header, const char *string, const char *instr)
 
 char	*get_instruction(t_header *header, const char *string)
 {
+	int	flag_name;
+	int flag_comment;
 //	int 		start;
 //	int 		i;
-//
 
+	flag_name = 0;
+	flag_comment = 0;
 //	i = 0;
 //	while(ft_isspace(string[i]))
 //		i++;
 //	start = i;
 	//int chk = ft_strstr(string, NAME_CMD_STRING);
-//	if (ft_strstr(string, NAME_CMD_STRING))
-//		printf("\n->%s<-\n", ft_strstr(string, NAME_CMD_STRING));
-//	else
-//		printf("\n->%s<-\n", ft_strstr(string, COMMENT_CMD_STRING));
+	if (ft_strstr(string, NAME_CMD_STRING))
+		;
+	else
+		;
 
 	int ch;
-	int ch1;
+	//int ch1;
+
 
 	ch = check_name(header, string, NAME_CMD_STRING);
-	ch1 = check_name(header, string, COMMENT_CMD_STRING);
+	//ch1 = check_name(header, string, COMMENT_CMD_STRING);
 
-	printf("check = >%d< Name = >%s<\t>%s<\n", ch, header->comment, string);
+	printf("check = >%d< Name = >%s<\t>%s<\n", ch, header->prog_name, string);
 //	while(string[i])
 //	{
 //
