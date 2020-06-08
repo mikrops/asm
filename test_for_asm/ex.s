d29542658e4fc3b4936d431f438b8c96acc84fca
Bertrana <botediom@gmail.com>
2020-06-08 12:40:14 +0300

    rewrite ex.s to standart; del -Werrors from cmake; add new path to build configuration

--- test_for_asm/ex.s
+++ test_for_asm/ex.s
@@ -1,14 +1,1 @@
-.name "zork"
-.comment "just a basic living prog"
-
-	#jg
-	ld %0, 
-    st r4, 56
-    st r5, 510
-    zjmp %501
-    restgf:
-		 l2:
-	sti	r9,%:live, r7
-	and	r15,%0,r7
-live:	live	%1
-	zjmp%:live
+.name "zork".comment "just a basic living prog"l2:	sti	r1,%:live,%0	and	r1,%0,r1live:	live	%1	zjmp	%:live
